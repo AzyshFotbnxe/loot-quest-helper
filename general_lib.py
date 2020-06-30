b510d687d2f6695eadb66415685ccb15fb3223a4
sanosenx86 <44931458+sanosenx86@users.noreply.github.com>
2020-06-30 20:58:06 +0800

    Now have load item only implemented.

--- general_lib.py
+++ general_lib.py
@@ -8,16 +8,19 @@ KEYS={
     'INCREMENT': "f",
     'DECREMENT': "v",
     "ADVANCE": "ctrl",
-    "START": "="
+    "START": "=",
+    "ITEMONLY": "r"
 }
 class t_enter_code(threading.Thread):
-    def __init__(self, code, keys):
+    def __init__(self, code, keys, item_only=False, advance_only=False):
         threading.Thread.__init__(self)
         self.code=code
         self.keys=keys
+        self.only_advance=advance_only
+        self.item_only=item_only
 
     def run(self):
-        enter_code(self.code, self.keys)
+        enter_code(self.code, self.keys,self.item_only, self.only_advance)
 
 ## Copy and pasted from internet to stop a thread. 
 ## https://stackoverflow.com/questions/323972/is-there-any-way-to-kill-a-thread
@@ -38,15 +41,15 @@ def _async_raise(tid, exctype):
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, None)
         raise SystemError("PyThreadState_SetAsyncExc failed")
 
-def enter_code(code, keys):
+def enter_code(code, keys, item_only=False, only_advance=False):
     for char in code:
         if char in "0123456789":
             digit = int(char)
-            if digit > 5:
+            if digit > 5 and not only_advance:
                 pyautogui.typewrite([keys["DECREMENT"]] * (10-digit), interval=0.1)
             else:
                 pyautogui.typewrite([keys["INCREMENT"]] * digit, interval=0.1)
-            pyautogui.typewrite([keys["ADVANCE"]], interval = 0.1)
+            pyautogui.typewrite(1*bool(item_only)*[keys['ITEMONLY']]+[keys["ADVANCE"]])
 
 def filter_code(code):
     tmp=filter(lambda ch: ch in '0123456789-', code)
